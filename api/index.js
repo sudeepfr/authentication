14541bd639c22f66e66916f83d39905849e1a890
sudeepfr <sudeepverma111213@gmail.com>
2025-06-06 14:26:13 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const mongoose =require('mongoose');
+const dotenv=require('dotenv');
 const app=express();
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+     console.log("connected successfully ")
+}).catch((err)=>{
+    console.log(err); 
+}); 
+
+
 
 app.get('/',(req,res)=>{
      console.log("inside the get");
